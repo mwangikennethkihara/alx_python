08f986fde62a658ac1395cf5af2d5966854c83dc
kennethmwangikihara <khallistokayne@gmail.com>
2023-08-02 16:31:34 -0700

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,26 @@
+# 0-square.py
+
+"""This is a module-level documentation.
+
+This module contains a class called Square, which defines a square by its size.
+
+"""
+
+class Square:
+    """Represents a square.
+
+    This class defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
