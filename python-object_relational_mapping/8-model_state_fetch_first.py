85e31fdbc1f2b13df4de476a07761073011c6e9b
kennethmwangikihara <khallistokayne@gmail.com>
2023-08-22 04:03:53 -0700

    python-object_relational_mapping

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
-"""
-Script that prints the first State object from the database hbtn_0e_6_usa
-"""
+# 8-model_state_fetch_first.py
+"""import a model and fitch the first row"""
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (argv[1],
+         argv[2],
+         argv[3]),
+        pool_pre_ping=True)
 
-    # Database connection setup
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, database_name), pool_pre_ping=True)
-
-    # Create a configured "Session" class
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
     session = Session()
 
-    # Query and display the first State object
-    first_state = session.query(State).order_by(State.id).first()
-
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
-    else:
+    if session.query(State).count() == 0:
         print("Nothing")
+
+    state = session.query(State).first()
+
+    print('{}: {}'.format(state.id, state.name))
 
     session.close()
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""
-This script lists all State objects
-that contain the letter `a`
-from the database `hbtn_0e_6_usa`.
-"""
+# 9-model_state_filter_a.py
+"""import a model and fitch the states where it has letetr a"""
 
 from sys import argv
-from model_state import State, Base
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (argv[1],
+         argv[2],
+         argv[3]),
+        pool_pre_ping=True)
 
-    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    states = session.query(State).filter(State.name.contains('a'))
-    if states is not None:
-        for state in states:
-            print('{0}: {1}'.format(state.id, state.name))
+    states = session.query(State).filter(State.name.like('%a%'))
+
+    for i in states:
+        print('{}: {}'.format(i.id, i.name))
+
+    session.close()
