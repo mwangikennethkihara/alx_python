93066c54685780e6412d23466f5ee3846a8e56e3
kennethmwangikihara <khallistokayne@gmail.com>
2023-10-11 07:38:00 -0700

    api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,42 @@
+""" Uses a REST API, fetching data about a given employee
+    (identified by their id passed in as an arg).
+    Returns:
+            information about thir TODO list progress
+"""
+
+import requests
+import sys
+
+
+base_url = "https://jsonplaceholder.typicode.com"
+employee_id = sys.argv[1]
+
+# Fetch employee details
+employee_url = "{}/users/{}".format(base_url, employee_id)
+employee_response = requests.get(employee_url)
+employee_data = employee_response.json()
+
+if 'name' not in employee_data:
+    print("Employee not found.")
+    sys.exit(1)
+
+employee_name = employee_data.get('name')
+
+# Fetch employee's TODO list
+todo_url = "{}/users/{}/todos".format(base_url, employee_id)
+todo_response = requests.get(todo_url)
+todo_data = todo_response.json()
+
+# Calculate progress
+total_tasks = len(todo_data)
+completed_tasks = sum(1 for task in todo_data if task.get("completed"))
+
+# Display progress
+print("Employee {} is done with tasks({}/{}):".format(employee_name,
+      completed_tasks, total_tasks))
+
+# Display completed task titles
+for task in todo_data:
+    if task.get("completed"):
+        formatted_task_title = "\t {}".format(task.get("title"))
+        print(formatted_task_title)
